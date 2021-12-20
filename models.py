3f52f7fe10b8ed49dfc11085fb07beffd22ffdf5
Thomas Roussel <thomas.roussel@ik.me>
2021-12-20 18:19:47 +0100

    draft tournament and player models

---
+++ models.py
@@ -0,0 +1,71 @@
+
+
+class Tournament:
+    def __init__(self, name, location, date, round_total, round_list, player_list, time_control, description):
+        self.name = name
+        self.location = location
+        self.date = date
+        self.round_total = round_total
+        self.round_list = round_list
+        self.player_list = player_list
+        self.time_control = time_control
+        self.description = description
+
+    def __repr__(self):
+        return f"Tournoi {self.name} du {self.date} à {self.location} en {self.round_total} tours, " \
+               f"mode {self.time_control} \n" \
+               f"{self.description} \n " \
+               f"Détails des tours : {self.round_list} \n " \
+               f"Liste des joueurs : {self.player_list}"
+
+    @classmethod
+    def create_tournament(cls):
+        return cls(
+            name=input("Nom du tournoi : "),
+            location=input("Lieu du tournoi : "),
+            date=input("Date du tournoi (JJ/MM/AA) : "),
+            round_total=str(4),
+            round_list=[],
+            player_list=[],
+            time_control=input("Bullet, Blitz ou Coup rapide : "),
+            description=input("Description du tournoi : ")
+        )
+    
+    def save_tournament(self):
+        pass
+
+
+class Player:
+    def __init__(self, last_name, first_name, date_of_birth, gender, ranking):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.date_of_birth = date_of_birth
+        self.gender = gender
+        self.ranking = ranking
+
+    def __repr__(self):
+        return f"{self.last_name} {self.first_name}, né le {self.date_of_birth}, {self.gender}, classé {self.ranking}"
+
+    @classmethod
+    def create_player(cls):
+        return cls(
+            last_name=input("Nom de famille : "),
+            first_name=input("Prénom : "),
+            date_of_birth=input("Date de naissance JJ/MM/AAA : "),
+            gender=input("Sexe : "),
+            ranking=input("Classement : ")
+        )
+
+
+class Round:
+    def __init__(self, match_list):
+        self.match_list = match_list
+        
+    
+class Match:
+    def __init__(self, player_pair, result):
+        self.player_pair = player_pair
+        self.result = result
+        
+    def input_result(self):
+        pass
