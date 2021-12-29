6c6d923121a8951049bb04d4ecee0b54cca7f5fd
Thomas Roussel <thomas.roussel@ik.me>
2021-12-29 11:23:23 +0100

    serialize func

--- models/player.py
+++ models/player.py
@@ -13,3 +13,12 @@ class Player:
 
     def __str__(self):
         return f"{self.last_name} {self.first_name} ({self.date_of_birth}, {self.gender}, {self.ranking})"
+
+    def serialize_player(self):
+        return {
+            "last_name": self.last_name,
+            "first_name": self.first_name,
+            "date_of_birth": self.date_of_birth,
+            "gender": self.gender,
+            "ranking": self.ranking
+        }
