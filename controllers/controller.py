2f07d9f27b1c36e21a9fc2f8ca33962b0c5f1952
Thomas Roussel <thomas.roussel@ik.me>
2022-01-18 18:31:10 +0100

    new controller version

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,6 +2,7 @@ from models.database import DataBase
 from views.views import View
 from models.tournament import Tournament
 from models.player import Player
+from util import NUMBER_OF_PLAYERS, CREATE_TOURNAMENT, LOAD_PREVIOUS_STATE, GENERATE_ROUND, QUIT
 
 
 class Controller:
@@ -14,35 +15,39 @@ class Controller:
 
     def run(self):
 
-        if self.view.input_main() == str(1):
-            current_tournament = self.instantiate_tournament()
-            self.table_tournaments.insert(current_tournament.serialize_tournament())
+        operation = self.view.input_main()
 
-            while len(current_tournament.player_list) < 8:
-                current_tournament.player_list.append(self.add_player())
+        while operation != QUIT:
 
-            for player in current_tournament.player_list:
-                self.table_players.insert(player.serialize_player())
+            if operation == CREATE_TOURNAMENT:
+                current_tournament = self.instantiate_tournament()
 
-            sorted_players = sorted(current_tournament.player_list, key=lambda player: player.ranking, reverse=True)
-            upper_half = sorted_players[:len(sorted_players)//2]
-            lower_half = sorted_players[len(sorted_players)//2:]
+                while len(current_tournament.player_list) < NUMBER_OF_PLAYERS:
+                    current_player = self.add_player()
+                    current_tournament.player_list.append(current_player)
+                    self.table_players.insert(current_player.serialize_player())
 
-            current_tournament.round_list.append(dict(zip(upper_half, lower_half)))
+                self.table_tournaments.insert(current_tournament.serialize_tournament())
 
-            print(current_tournament)
+                print(current_tournament)
 
-        elif self.view.input_main() == str(2):
-            pass
+            if operation == GENERATE_ROUND:
+                current_tournament.round_list.append(self.matching_player(current_tournament.player_list))
 
-        else:
-            self.view.end_program()
+        self.view.end_program()
 
     def instantiate_tournament(self):
         return Tournament(*self.view.input_tournament())
 
     def add_player(self):
         return Player(*self.view.input_player())
+
+    def matching_player(self, player_list):
+        sorted_players = sorted(player_list, key=lambda player: (player.score, player.ranking), reverse=True)
+        upper_half = sorted_players[:len(sorted_players) // 2]
+        lower_half = sorted_players[len(sorted_players) // 2:]
+
+        return dict(zip(upper_half, lower_half))
 
     def load_all_data(self):
         self.db.all()
