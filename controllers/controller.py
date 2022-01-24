dcba36dbc119eccf1f0b6e3db0970abcf1f74fb9
Thomas Roussel <thomas.roussel@ik.me>
2022-01-24 19:35:24 +0100

    updated

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,8 @@
 from views.views import View
 from models.tournament import Tournament
 from models.player import Player
+from models.round import Round
+from models.match import Match
 from util import *
 
 
@@ -29,14 +31,22 @@ class Controller:
 
             elif operation == GENERATE_ROUND:
                 current_tournament = self.load_tournament()
-                self.match_player(current_tournament.player_list)
+
+                current_tournament.player_list = self.load_players(current_tournament)
+                current_tournament.round_list = self.match_players(current_tournament.player_list)
+
+                table_rounds.insert(current_tournament.round_list)
 
                 print(current_tournament)
+
+            elif operation == ENTER_RESULTS:
+                self.update_scores()
 
             elif operation == LOAD_PREVIOUS_STATE:
                 current_tournament = self.load_tournament()
-                print(current_tournament)
-                self.load_players(current_tournament)
+
+                current_tournament.player_list = self.load_players(current_tournament)
+                current_tournament.round_list = self.load_rounds(current_tournament)
 
                 print(current_tournament)
 
@@ -47,21 +57,32 @@ class Controller:
     def create_tournament(self):
         return Tournament(*self.view.input_tournament())
 
-    def load_tournament(self):
-        return Tournament(**(table_tournament.all()[-1]))
-
     def add_player(self):
         return Player(*self.view.input_player())
 
-    def load_players(self, tournament):
-        players_from_db = table_players.all()
-        for player in players_from_db:
-            return tournament.player_list.append(Player(**player))
+    def load_tournament(self):
+        return Tournament(**(table_tournament.all()[-1]))
 
-    def match_player(self, player_list):
-        sorted_players = sorted(player_list, key=lambda player: (player.score, player.ranking), reverse=True)
+    def load_players(self, tournament):
+        players_from_db = (table_players.all()[-8:])
+        for player in players_from_db:
+            tournament.player_list.append(Player(**player))
+        return tournament.player_list
+
+    def load_rounds(self, tournament):
+        rounds_from_db = (table_rounds.all())
+        tournament.round_list.append(rounds_from_db)
+        return tournament.round_list
+
+    def sort_players(self, player_list):
+        return sorted(player_list, key=lambda player: (player.score, player.ranking), reverse=True)
+
+    def match_players(self, player_list):
+        sorted_players = self.sort_players(player_list)
         upper_half = sorted_players[:len(sorted_players) // 2]
         lower_half = sorted_players[len(sorted_players) // 2:]
 
-        return dict(zip(upper_half, lower_half))
+        return list(zip(upper_half, lower_half))
 
+    def update_scores(self):
+        self.view.enter_results()
