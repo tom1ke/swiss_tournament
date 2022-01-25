0da688723a6f357a2ea7ea938a24e7b11fb603db
Thomas Roussel <thomas.roussel@ik.me>
2022-01-25 18:57:13 +0100

    in progress, not functional

--- models/match.py
+++ models/match.py
@@ -2,17 +2,19 @@
 
 class Match:
 
-    def __init__(self, player_1=None, player_2=None):
+    def __init__(self, player_1=None, player_2=None, player_1_result=0, player_2_result=0):
         if player_1 is None:
             player_1 = []
         if player_2 is None:
             player_2 = []
         self.player_1 = player_1
         self.player_2 = player_2
-        self.player_pair = (self.player_1, self.player_2)
+        self.player_1_result = player_1_result
+        self.player_2_result = player_2_result
+        self.player_pair = ([self.player_1, self.player_1_result], [self.player_2, self.player_2_result])
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return f"{self.player_1} contre {self.player_2}"
+        return f"{self.player_1} ({self.player_1_result}) contre {self.player_2} ({self.player_2_result})"
