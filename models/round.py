ad05c8c4df82ce03d4742fc3fd3e45ec9d62ffb7
Thomas Roussel <thomas.roussel@ik.me>
2022-01-26 13:52:21 +0100

    init str serialize updated

--- models/round.py
+++ models/round.py
@@ -2,11 +2,15 @@ from util import NOW
 
 
 class Round:
-    def __init__(self, name, match_list=None, start_time=NOW, end_time=None, completed=False):
+    def __init__(self, name, match_list=None, start_time=None, end_time=None, completed=None):
         if match_list is None:
             match_list = []
+        if start_time is None:
+            start_time = NOW
         if end_time is None and completed:
             end_time = NOW
+        if completed is None:
+            completed = False
         self.name = name
         self.match_list = match_list
         self.start_time = start_time
@@ -17,17 +21,22 @@ class Round:
         return self.__str__()
 
     def __str__(self):
+        nl = "\n"
         if self.end_time is None:
             self.end_time = "Toujours en cours"
         return f"\n{self.name} \n" \
                f"Début : {self.start_time} \n" \
-               f"Matchs : {self.match_list} \n" \
+               f"Matchs : \n{(nl.join(map(str, self.match_list)))} \n" \
                f"Fin : {self.end_time} \n"
 
     def serialize_round(self):
         return {
             "name": self.name,
+            "match_list": self.match_list,
             "start_time": self.start_time,
             "end_time": self.end_time,
             "completed": self.completed
         }
+
+    def serialize_match_list(self):
+        pass
