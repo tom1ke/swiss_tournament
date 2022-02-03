366440219788ac2798e9d3333010542d1120303f
Thomas Roussel <thomas.roussel@ik.me>
2022-02-03 19:13:28 +0100

    docstrings

--- util.py
+++ util.py
@@ -2,63 +2,68 @@ from datetime import datetime
 from tinydb import TinyDB
 
 """
-Database setup
+Initialisation de la base de données TinyDB. 
+Le fichier .json est créé à la racine du projet 
+Il contient une table de tournois et une table de joueurs
 """
 DB = TinyDB("data.json", indent=4)
 TABLE_TOURNAMENTS = DB.table("Tournament")
 TABLE_PLAYERS = DB.table("Players")
-TABLE_ROUNDS = DB.table("Rounds")
 
 """
-Tournament setup
+Caractéristiques du tournoi (nombre de tours et de joueurs)
 """
 NUMBER_OF_ROUNDS = 4
 NUMBER_OF_PLAYERS = 8
 
 """
-Date & time setup
+Date & heure
 """
 
 
 def get_time():
+    """
+    Récupère la date et l'heure actuelles
+    :return: Chaîne de caractère formatée de la date et l'heure
+    """
     return datetime.now().isoformat(' ', 'seconds')
 
 
 """
-Main menu options
+Variables correspondant aux options du menu principal
 """
-CREATE_TOURNAMENT = str(1)
-GENERATE_ROUND = str(2)
-ENTER_RESULTS = str(3)
-LOAD_PREVIOUS_STATE = str(4)
-REPORTS = str(5)
-QUIT = str(6)
+CREATE_TOURNAMENT = "1"
+GENERATE_ROUND = "2"
+ENTER_RESULTS = "3"
+LOAD_PREVIOUS_STATE = "4"
+REPORTS = "5"
+QUIT = "6"
 
 """
-Time control options
+Variables correspondant aux options du menu de format de temps
 """
-BLITZ = str(1)
-BULLET = str(2)
-RAPID = str(3)
+BLITZ = "1"
+BULLET = "2"
+RAPID = "3"
 
 """
-Reports menu options
+Variables correspondant aux options du menu des rapports
 """
-ALL_ACTORS = str(1)
-TOURNAMENTS = str(2)
-PLAYERS = str(3)
-ROUNDS = str(4)
-MATCHES = str(5)
-MAIN_MENU = str(6)
+ALL_ACTORS = "1"
+TOURNAMENTS = "2"
+PLAYERS = "3"
+ROUNDS = "4"
+MATCHES = "5"
+MAIN_MENU = "6"
 
 """
-Round state menu options
+Variables correspondant aux options du menu de statut de tour
 """
-COMPLETED = str(1)
-ONGOING = str(2)
+COMPLETED = "1"
+ONGOING = "2"
 
 """
-Display mode menu options
+Variables correspondant aux options du menu de classement des données des rapports
 """
-SORT_ALPHA = str(1)
-SORT_RANK = str(2)
+SORT_ALPHA = "1"
+SORT_RANK = "2"
